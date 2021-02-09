0bd78801f19e1066f3a69fa5ada1274c2ce6b749
Arnav Gosain <arnav@arnavgosain.com>
2021-02-09 13:28:15 +0530

    Self-host font
    Signed-off-by: Arnav Gosain <arnav@arnavgosain.com>

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,14 @@
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
-import GoogleFonts from "next-google-fonts";
 import "~/styles/tailwind.css";
 import "~/styles/index.css";
 import "~/styles/tailwind-utils.css";
 import SEO from "~/next-seo.config";
+import "@fontsource/inter"
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" />
       <DefaultSeo {...SEO} />
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
