86e4edb357c0cb70469c8bdcfc58b1b942c393ca
Arnav Gosain <arnav@arnavgosain.com>
2021-05-01 00:43:30 +0530

    :tada: Add blog posts to index page
    Signed-off-by: Arnav Gosain <arnav@arnavgosain.com>

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,14 +1,15 @@
 import { HiArrowRight } from "react-icons/hi";
 import Link from "next/link";
 import Image from "next/image";
+import { getDateObjectFromString } from "~/lib/utils";
 
 export default function PostCard({ data }) {
   return (
     <Link href={`/${data.slug}`}>
-      <div className="cursor-pointer px-4 w-full py-4 bg-gray-100 dark:bg-gray-900 rounded-md border border-gray-200 dark:border-gray-800 shadow-md flex items-center justify-between group">
+      <a className="px-4 w-full py-4 bg-gray-100 hover:bg-gray-200 ease-out transition duration-100 dark:bg-gray-900 rounded-md border border-gray-300 dark:border-gray-800 shadow-md flex items-center justify-between group dark:hover:bg-gray-800">
         <div className="flex flex-col space-y-4">
           <h1 className="text-lg font-semibold">{data.title}</h1>
-          <div className="flex items-center justify-start space-x-2">
+          <div className="flex items-center justify-start space-x-4">
             <Image
               src="/images/display.jpg"
               height={32}
@@ -19,13 +20,20 @@ export default function PostCard({ data }) {
             />
             <h2>{data.author}</h2>
             <div>/</div>
-            <h2>Published on {data.date}</h2>
+            <p>
+              Published on{" "}
+              <time dateTime={getDateObjectFromString(data.date).toISOString()}>
+                {data.formattedDate}
+              </time>
+            </p>
+            <div>/</div>
+            <h2>{data.readingTime}</h2>
           </div>
         </div>
         <Link href={`/${data.slug}`}>
-          <HiArrowRight className="h-5 w-5 group-hover:animate-bounce" />
+          <HiArrowRight className="h-5 w-5" />
         </Link>
-      </div>
+      </a>
     </Link>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,53 @@
 import IntroSection from "~/components/homepage/sections/Intro";
 import PageLayout from "~/components/PageLayout";
-// import PhotographySection from "~/components/homepage/sections/photography";
 import ProjectsSection from "~/components/homepage/sections/projects";
+import PostCard from "~/components/PostCard";
+import { getAllPosts, getSlugData } from "~/lib/notion";
+import { HiArrowRight } from "react-icons/hi";
+import Link from "next/link";
 
-export default function IndexPage() {
+export default function IndexPage({ posts }) {
   return (
     <>
-      <PageLayout className="items-center justify-center" noFooter>
-        <div className="flex flex-col gap-8 lg:px-5">
+      <PageLayout>
+        <div className="flex flex-col space-y-16 pt-8 pb-6">
           <IntroSection />
+          <div className="flex flex-col space-y-6 w-full">
+            <div className="flex items-center justify-between">
+              <span className="text-3xl font-bold dark:text-white">
+                Blog posts
+              </span>
+              <Link href="/blog">
+                <a className="dark:bg-gray-800 dark:hover:bg-gray-700 bg-gray-100 border border-gray-300 hover:bg-gray-200 dark:border-gray-800 flex items-center justify-center transition duration-150 ease-in space-x-2 py-2 px-2 rounded-md">
+                  <span>View all posts</span>
+                  <HiArrowRight />
+                </a>
+              </Link>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              {posts.map((item) => {
+                return <PostCard key={item.id} data={item} />;
+              })}
+            </div>
+          </div>
           <ProjectsSection />
         </div>
       </PageLayout>
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const posts = [];
+
+  for (const { slug } of await getAllPosts()) {
+    const { metadata } = await getSlugData(slug);
+    posts.push(metadata);
+  }
+
+  return {
+    props: {
+      posts: posts.slice(0, 6),
+    },
+  };
+};
