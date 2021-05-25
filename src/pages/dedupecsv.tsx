572eb66fb62d9950450baeb2193f3a6238dffe3b
Arnav Gosain <arnav@arnavgosain.com>
2021-05-25 07:27:56 +0530

    build: add commitizen

--- src/pages/dedupecsv.tsx
+++ src/pages/dedupecsv.tsx
@@ -1,3 +1,25 @@
+import { NextSeo } from "next-seo";
+
 export default function DedupeCsvPage(): JSX.Element {
-  return null;
+  return (
+    <>
+      <NextSeo
+        title="OneTab Export to CSV Converter"
+        description="Convert OneTab Export text to CSV for importing into Notion."
+        openGraph={{
+          title: "OneTab Export to CSV Converter",
+          description:
+            "Convert OneTab Export text to CSV for importing into Notion.",
+        }}
+      />
+      <style jsx scoped>
+        {`
+          .gradient {
+            background-image: url("/static/dedupecsv/mesh.png");
+          }
+        `}
+      </style>
+      <div className="w-screen h-screen overflow-hidden gradient"></div>
+    </>
+  );
 }
