91c471a85025c3c2395a42fd0be1a2d9981d2e38
Arnav Gosain <arnav@arnavgosain.com>
2021-07-07 00:55:48 +0530

    Tweak next-themes options

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,15 +1,21 @@
-import "../styles/index.css";
-import "react-notion-x/src/styles.css";
 import { DefaultSeo } from "next-seo";
 import { ThemeProvider } from "next-themes";
+import "react-notion-x/src/styles.css";
 import SEO from "~/next-seo.config";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <style data-href="https://fonts.googleapis.com/css2?family=Inter"></style>
       <DefaultSeo {...SEO} />
-      <ThemeProvider attribute="class">
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        themes={["light", "dark"]}
+        enableColorScheme={false}
+        enableSystem={false}
+      >
         <Component {...pageProps} />
       </ThemeProvider>
     </>
