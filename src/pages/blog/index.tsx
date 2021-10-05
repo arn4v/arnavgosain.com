dffe3a1f63c443a50a85629caa5e0ca19002918b
Arnav Gosain <arnav@arnavgosain.com>
2021-10-05 17:55:35 +0530

    Analytics

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -14,45 +14,41 @@ const seoConfig: SeoProps = {
 
 const ProjectsPage = ({ data }: { data: PostMetadata[] }) => {
   return (
-    <>
-      <PageLayout breadcrumb={{ Projects: "/projects" }} seo={seoConfig}>
-        <div className="flex flex-col gap-10">
-          <h1 className="text-3xl font-bold dark:text-white font-secondary hidden lg:block">
-            Blog
-          </h1>
-          <div className="flex flex-col items-center justify-center gap-4 lg:gap-6">
-            {data
-              .sort(
-                (a, b) =>
-                  new Date(b.published_on).valueOf() -
-                  new Date(a.published_on).valueOf()
-              )
-              .map((item) => {
-                return (
-                  <div
-                    key={`${item.title}-${item.slug}`}
-                    className="flex items-center justify-between w-full"
-                  >
-                    <Link href={`/${item.slug}`} passHref>
-                      <a className="text-orange-500 hover:text-orange-700 underline">
-                        {item.title}
-                      </a>
-                    </Link>
-                    <p className="self-center justify-self-end">
-                      Published on{" "}
-                      <time
-                        dateTime={new Date(item.published_on).toISOString()}
-                      >
-                        {format(new Date(item.published_on), "do MMMM yyyy")}
-                      </time>
-                    </p>
-                  </div>
-                );
-              })}
-          </div>
+    <PageLayout breadcrumb={{ Projects: "/projects" }} seo={seoConfig}>
+      <div className="flex flex-col gap-10">
+        <h1 className="text-3xl font-bold dark:text-white font-secondary hidden lg:block">
+          Blog
+        </h1>
+        <div className="flex flex-col items-center justify-center gap-4 lg:gap-6">
+          {data
+            .sort(
+              (a, b) =>
+                new Date(b.published_on).valueOf() -
+                new Date(a.published_on).valueOf()
+            )
+            .map((item) => {
+              return (
+                <div
+                  key={`${item.title}-${item.slug}`}
+                  className="flex items-center justify-between w-full"
+                >
+                  <Link href={`/${item.slug}`} passHref>
+                    <a className="text-orange-500 hover:text-orange-700 underline">
+                      {item.title}
+                    </a>
+                  </Link>
+                  <p className="self-center justify-self-end">
+                    Published on{" "}
+                    <time dateTime={new Date(item.published_on).toISOString()}>
+                      {format(new Date(item.published_on), "do MMMM yyyy")}
+                    </time>
+                  </p>
+                </div>
+              );
+            })}
         </div>
-      </PageLayout>
-    </>
+      </div>
+    </PageLayout>
   );
 };
 
