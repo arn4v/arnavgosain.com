a6d290f64bc0617f4838421d3fd27d83f7777c51
Arnav Gosain <arnav@arnavgosain.com>
2022-01-08 00:32:51 +0530

    Fix build

--- src/pages/playlists.jsx
+++ src/pages/playlists.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Link from "~/components/CustomLink";
 import PageLayout from "~/components/PageLayout";
-import { baseUrl } from "~/constants";
+import { baseUrl, isProd } from "~/constants";
 
 /**
  * @param {Object} props
