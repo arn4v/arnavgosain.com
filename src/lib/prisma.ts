56587134667fc19f958357a749d032e58a44f966
Arnav Gosain <arnav@arnavgosain.com>
2022-03-05 21:59:56 +0530

    cache prisma client in dev mode

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,22 @@
 import { PrismaClient } from "@prisma/client";
+import { isProd } from "~/constants";
 
-export const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+if (isProd) {
+  prisma = new PrismaClient();
+  prisma.$connect();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+    global.prisma.$connect();
+  }
+
+  prisma = global.prisma;
+}
+
+export { prisma };
