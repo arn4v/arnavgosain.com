7c66645b2147c9cdd1f7d986dba78eb665dc2e8c
Arnav Gosain <arnav@arnavgosain.com>
2023-02-16 22:55:10 +0530

    Fix char width

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -30,7 +30,7 @@ const BlogPage = ({ data }: { data: Post[] }) => {
 								<Link
 									href={`/${item.slug}`}
 									passHref
-									className="text-emerald-800 hover:underline lg:text-xl font-semibold font-serif leading-6 tracking-wide"
+									className="text-emerald-800 hover:underline lg:text-xl font-semibold font-serif leading-6 tracking-tight"
 								>
 									{item.title}
 								</Link>
