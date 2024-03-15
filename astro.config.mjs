045acc5bb2a83f9808bfe80250f7f4306ad5daf3
Arnav Gosain <arnav@arnavgosain.com>
2024-03-15 17:57:36 +0530

    Update sitemap/robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react()]
+  site: "https://arnavgosain.com",
+  integrations: [tailwind(), sitemap(), react(), mdx()],
 });
