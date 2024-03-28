646b8904f0e49810fb068018c7df2979b97bc26a
Arnav Gosain <arnav@arnavgosain.com>
2024-03-28 18:57:50 +0530

    Add beam

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly BEAM_TOKEN?: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
